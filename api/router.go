e4ac6accf57be0e3f87e0271a4e9141720927284
solace06 <khushi.sharma061101@gmail.com>
2026-08-09 21:58:53 +0530

    add api versioning

--- api/router.go
+++ api/router.go
@@ -1,1 +1,17 @@
 package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/solace06/auth-service/internal/user"
+)
+
+func NewRouter() *gin.Engine {
+
+	r := gin.Default()
+
+	v1 := r.Group("/api/v1")
+
+	user.UserRoutes(v1)
+
+	return r
+}
--- internal/user/models.go
+++ internal/user/models.go
@@ -1,6 +1,16 @@
 package user
 
-
 type User struct {
-	
+	ID           int64  `json:"id"`
+	Name         string `json:"name"`
+	Email        string `json:"email"`
+	PasswordHash string `json:"-"`
+	CreatedAt    string `json:"created_at"`
+	UpdatedAt    string `json:"updated_at"`
+}
+
+type CreateUserRequest struct {
+	Name     string `json:"name" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required"`
 }
