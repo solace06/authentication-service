0ad0e34b8f69c789ec6d89e4643173e6d3e3d4e7
solace06 <khushi.sharma061101@gmail.com>
2026-08-11 00:49:29 +0530

    add password hashing

--- pkg/hash.go
+++ pkg/hash.go
@@ -1,1 +1,13 @@
 package pkg
+
+import "golang.org/x/crypto/bcrypt"
+
+func PasswordHash(password string) (string, error) {
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+
+	return string(hash), nil
+}
