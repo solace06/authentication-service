fdad990579555f58b994d105b817be334acfda85
solace06 <khushi.sharma061101@gmail.com>
2026-08-14 21:21:39 +0530

    implement basic token bucket rate limiter

--- api/middleware.go
+++ api/middleware.go
@@ -1,1 +1,40 @@
 package api
+
+import "time"
+
+type Limiter struct {
+	Capacity float64
+	Rate     float64
+	Tokens   float64
+	Last     time.Time
+}
+
+
+func NewLimiter(capacity int, rate float64) *Limiter {
+	return &Limiter{
+		Capacity: float64(capacity),
+		Rate: rate,
+		Tokens: float64(capacity),
+		Last: time.Now(),
+	}
+}
+
+func (l *Limiter) Allow() bool {
+	//calc time elapsed
+	now:=time.Now()
+	elapsed:=now.Sub(l.Last).Seconds()
+
+	//update limiter
+	l.Tokens = min(l.Tokens+(l.Rate*elapsed),l.Capacity)
+	l.Last=now
+
+	//request rejected
+	if l.Tokens < 1 {
+		return false
+	}
+
+	//request accepted
+	l.Tokens--
+
+	return true
+}
